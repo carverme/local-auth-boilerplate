7adb907693057914376bf54755891ff5836a0afd
Matt Carver <mattcarver.wd@gmail.com>
2018-06-21 17:02:40 -0700

    authentication boilerplate setup

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
 var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('./config/passportConfig');
+var isLoggedIn = require('./middleware/isLoggedIn');
+var flash = require('connect-flash');
+
 var app = express();
 
 app.set('view engine', 'ejs');
@@ -9,11 +15,34 @@ app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 
+//1. This must come before you use app.use passport.
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+
+//2. This initializes flash... setup flash messages.
+app.use(flash());
+
+//3. This must come after the session setup.
+app.use(passport.initialize());
+app.use(passport.session());
+
+//4. Attach the currentUser to the response for all routes.
+//Also attaches the flash messages.
+app.use(function(req, res, next) {
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
+  next();
+});
+
+//Routes
 app.get('/', function(req, res) {
   res.render('index');
 });
 
-app.get('/profile', function(req, res) {
+app.get('/profile', isLoggedIn, function(req, res) {
   res.render('profile');
 });
 
